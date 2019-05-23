d2bbd56aa0e356bac29cdbe06d2fb5b5e901a3bc
Sahil Jain <31558818+j105sahil@users.noreply.github.com>
2019-05-23 15:45:06 +0530

    Update Bluetooth.ino

--- Blue_pill/Bluetooth.ino
+++ Blue_pill/Bluetooth.ino
@@ -1,38 +1,31 @@
-//PROGRAM FOR BLUETOOTH INTERFACE WITH STM32F103C8
-//CIRCUIT DIGEST
-//Pramoth Thangavel
- 
-const int pinout = PC13; // declare pinout with int data type and pin value
- 
-String inputdata="";
- 
-void setup()
-{
-    Serial.begin(9600);                      //Sets the baud rate for bluetooth pins                      
-    pinMode(pinout, OUTPUT);                  //Sets digital pin PC13 as output pin for led
-    Serial.print("HELLO WORLD\n");
-    Serial.print("BLUETOOTH WITH STM32\n");
+String string ="";
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+delay(10000);
 }
  
-void loop()
-{
-   if(Serial.available() > 0)      // Send data only when you receive data:
-   {
-      inputdata = Serial.readString();        //Read the incoming data & store into data
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+ Serial.print("Enter the string:");
+while(1){
+
+    if(Serial.available()>0){
+      int k=0;
+  
+      string=Serial.readString();
+      Serial.print("\n");
+  
+    while(string[k]){
+      Serial.print(string[k]);
+      Serial.print("\t");
+      k++;
+      delay(1000);
+    }
+    break;
+    }
+ }
+
  
-           int k=0;
-    while(inputdata[k] && k<25){
-    
-   Serial.print("\n");
-   
-   Serial.print(inputdata[k]);
-   
-   if(inputdata[k]>='A' && inputdata[k]<='Z')
-    digitalWrite(pinout, LOW);
-   else
-    digitalWrite(pinout, HIGH);
-   k++;
-   delay(1000);
-  }
-   }
 }
